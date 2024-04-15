9a116d2030c067d1ff390d317c49cd3f5a8e96b0
gau-01 <167078118+gau-01@users.noreply.github.com>
2024-04-15 15:45:24 +0530

    Type Casting

---
+++ Casting.py
@@ -0,0 +1,58 @@
+# Casting /Conversion 
+# Convert data into one datatype to other
+
+x=13
+# print value of x and check datatype
+print(x)            
+print(type(x))
+# covert int into float, string, bool & complex  and check datatype
+x1= float(x)
+print(x1)
+print(type(x1))
+
+x2= str(x)
+print(x2)
+print(type(x2))
+
+x3= bool(x)
+print(x3)
+print(type(x3))
+
+x4= complex(x)
+print(x4)
+print(type(x4))
+print("...................")
+
+y=5.67
+print(type(y))
+# convert float datatype to other
+print(int(y))
+print(bool(y))
+print(str(y))
+print(complex(y))
+print(".....................")
+
+z="Data Science"
+print(type(z))
+# convert string datatype to other
+print(bool(z))
+#print(int(z))         ...N0 conversion possible
+#print(float(z))
+#print(complex(z))
+print(".....................")
+
+a=True
+print(type(a))
+# convert Bool datatype to other
+print(int(a))
+print(float(a))
+print(str(a))
+print(complex(a))
+print(".................")
+
+
+
+
+
+
+
