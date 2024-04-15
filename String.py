6118a0e0b7205aaa9dc11ebdbcb662c497580b08
gau-01 <167078118+gau-01@users.noreply.github.com>
2024-04-15 15:46:35 +0530

    String in python

---
+++ String.py
@@ -0,0 +1,22 @@
+# String - Combination of char, int, special character in " " or' '
+# Example
+
+y="Hello_Everyone@2346"
+z='abcd23'
+
+# Access single element in string By Indexing
+# Syntax : variblename[index]
+# Example
+
+y="Hello_Everyone@2346"
+# print y 
+print(y[10])
+# print o
+print(y[4])
+
+
+
+
+
+ 
+
