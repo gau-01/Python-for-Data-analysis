09ccbb39e415e5d227096596348640cf694be926
gau-01 <167078118+gau-01@users.noreply.github.com>
2024-04-15 15:43:41 +0530

    variable in python

---
+++ varibles.py
@@ -0,0 +1,17 @@
+# Syntax of varible
+# VariableName=data/Expression
+
+# Examples
+
+x=345       # int variable
+y="Gauri"   # string varible
+z=8.87      # Float varible
+w= True     # bool varible
+u= 87+65    # Expression
+
+# Display Variables
+
+A="Python"
+print(A)
+C= 34+76 
+print(C)
